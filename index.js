9cd0d708777d4a7b0a3d844d3aa0c459e652de73
Michael Uranaka <michael_uranaka@datahouse.com>
2014-09-09 17:12:02 -1000

    Adding updated utilities. Removing old unused utilities.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // reference all the utility sub-modueles.
-exports.stringUtilities       = require('./lib/string-utilities');
-exports.dominoUtilities       = require('./lib/domino-utilities');
-exports.errorUtilities        = require('./lib/error-utilities');
-exports.formUtilities         = require('./lib/form-utilities');
-exports.logUtilities          = require('./lib/log-utilities');
-exports.responseUtilities     = require('./lib/response-utilities');
-exports.as400Utilities        = require('./lib/as400-utilities');
+exports.StringUtils       		= require('./lib/string-utilities');
+exports.ErrorUtils        		= require('./lib/error-utilities');
+exports.FormUtils        		= require('./lib/form-utilities');
+exports.LogUtils       			= require('./lib/log-utilities');
+exports.ResponseUtils     		= require('./lib/response-utilities');
+exports.NumberUtils     		= require('./lib/number-utilities');

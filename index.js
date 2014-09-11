17f3189241773c230b325aaf26e10cdb5844746d
Michael Uranaka <michael_uranaka@datahouse.com>
2014-09-10 14:54:04 -1000

    Removing log utilities.

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 exports.StringUtils       		= require('./lib/string-utilities');
 exports.ErrorUtils        		= require('./lib/error-utilities');
 exports.FormUtils        		= require('./lib/form-utilities');
-exports.LogUtils       			= require('./lib/log-utilities');
 exports.ResponseUtils     		= require('./lib/response-utilities');
 exports.NumberUtils     		= require('./lib/number-utilities');

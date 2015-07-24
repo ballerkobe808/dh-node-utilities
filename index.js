8104073295cb080db695ff67986f6b4aade1cf4b
Michael Uranaka <michael_uranaka@datahouse.com>
2015-07-24 11:11:14 -1000

    adding sql utils.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // reference all the utility sub-modueles.
 exports.StringUtils       		= require('./lib/string-utilities');
 exports.ErrorUtils        		= require('./lib/error-utilities');
-exports.FormUtils        		= require('./lib/form-utilities');
+exports.FormUtils        		  = require('./lib/form-utilities');
 exports.ResponseUtils     		= require('./lib/response-utilities');
-exports.NumberUtils     		= require('./lib/number-utilities');
+exports.NumberUtils     		  = require('./lib/number-utilities');
 exports.MongoUtils     		    = require('./lib/mongo-utilities');
+exports.SQLUtils              = require('./lib/sql-utilities');

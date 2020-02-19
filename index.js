da3f5d55baeb8c273ded40eae7ac50c201efad8a
Michael Uranaka <michael_uranaka@datahouse.com>
2020-02-18 17:09:33 -1000

    updates and minor bug fixes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// reference all the utility sub-modueles.
+'use strict';
+
+// reference all the utility sub-modules.
 exports.StringUtils       		= require('./lib/string-utilities');
 exports.ErrorUtils        		= require('./lib/error-utilities');
 exports.ResponseUtils     		= require('./lib/response-utilities');
 exports.NumberUtils     		  = require('./lib/number-utilities');
-exports.MongoUtils     		    = require('./lib/mongo-utilities');
 exports.SQLUtils              = require('./lib/sql-utilities');
 exports.DateUtils             = require('./lib/date-utilities');
 exports.FileUtils             = require('./lib/file-utilities');
